e7cae8e043ef0441733165f75a19c11ca44e2466
dexter210 <78829953+dexter210@users.noreply.github.com>
2021-02-10 20:47:22 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+var name = prompt("what is your name");
+var color = prompt("what is your favorite color?");
+document.body.querySelector(".message").innerHTML="My name is "+name+". My favorite color is "+color;
